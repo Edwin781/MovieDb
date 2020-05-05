1b6c3efd3dd5a9c400a065078c8e80133e4b0c8d
Edwin Nnorom <nnoromedwin@gmail.com>
2020-05-05 13:20:56 -0500

    Updates

---
+++ precache-manifest.93d0c0e668aa6e9782c2ace6ba74df91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd718ac1e62491a2e5e62262fd9bb15",
+    "url": "/MovieDb/index.html"
+  },
+  {
+    "revision": "1bd08d3ad61881cc4f71",
+    "url": "/MovieDb/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a180b4bc488466ea2d7e",
+    "url": "/MovieDb/static/css/main.211b1682.chunk.css"
+  },
+  {
+    "revision": "1bd08d3ad61881cc4f71",
+    "url": "/MovieDb/static/js/2.64a07f73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MovieDb/static/js/2.64a07f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a180b4bc488466ea2d7e",
+    "url": "/MovieDb/static/js/main.735c9185.chunk.js"
+  },
+  {
+    "revision": "4b63fa6d92eba162507b",
+    "url": "/MovieDb/static/js/runtime-main.e4442c93.js"
+  },
+  {
+    "revision": "33c358faa8f7f099039deeebe26767ec",
+    "url": "/MovieDb/static/media/bg.33c358fa.png"
+  }
+]);
